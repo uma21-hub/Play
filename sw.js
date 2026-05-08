5694c015898c4ffbadb2f90783d7cd72f72ae286
uma21-hub <mpvvtumareddy@gmail.com>
2026-05-08 14:05:31 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('p4u-v1').then((c) => c.addAll(['/', '/index.html'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
